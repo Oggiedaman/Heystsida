d491e2a88c8e3be0978faa8b0421afb661306a03
MnsA <manala@edu.umea.se>
2015-06-17 12:29:51 +0200

    La till hitmarker.js

---
+++ hitmarker.js
@@ -0,0 +1,27 @@
+var hmPath = 'IMG/hitmarker.png';
+var hmDelay = 500;
+
+window.addEventListener('load', function(event) {
+	var button = document.getElementById('hitmarkerButton');
+	button.addEventListener('click', function(event) {
+		activateHitmarkers();
+	});
+});
+
+function activateHitmarkers() {
+	setInterval(function() {
+		var hm = document.createElement('img');
+		hm.src = hmPath;
+
+		hm.style.position = 'absolute';
+		hm.style.left = random(0, window.innerWidth) - hm.width / 2;
+		hm.style.top = random(0, window.innerHeight) - hm.height / 2;
+
+		document.body.appendChild(hm);
+	}, hmDelay);
+}
+
+// random in [min, max)
+function random(min, max) {
+	return min + Math.floor(Math.random() * (max - min));
+}
