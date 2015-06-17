a6f507f970a786bcffceb4c2639999f516406e2b
MnsA <manala@edu.umea.se>
2015-06-17 14:49:24 +0200

    hitmarker sound

--- hitmarker.js
+++ hitmarker.js
@@ -1,28 +1,33 @@
 var hmPath = 'IMG/hitmarker.png';
 var hmDelay = 125;
 var hmLifetime = 1000;
+var hmSoundElement; // load on start event
 
 window.addEventListener('load', function(event) {
 	var button = document.getElementById('hitmarkerButton');
+
+	hmSoundElement = document.createElement('audio');
+	hmSoundElement.src = 'SND/hitmarker.wav';
+
 	button.addEventListener('click', function(event) {
-		activateHitmarkers();
+		setInterval(spawnHitmarker, hmDelay);
 	});
 });
 
-function activateHitmarkers() {
-	setInterval(function() {
-		var hm = document.createElement('img');
-		hm.src = hmPath;
+function spawnHitmarker() {
+	var hm = document.createElement('img');
+	hm.src = hmPath;
 
-		hm.style.position = 'absolute';
-		//hm.style.clear = 'both';
-		hm.style.left = random(0, window.innerWidth - 2 * hm.width) + 'px';
-		hm.style.top = random(0, window.innerHeight - 2 * hm.height) + 'px';
+	hm.style.position = 'absolute';
+	//hm.style.clear = 'both';
+	hm.style.left = random(0, window.innerWidth - 2 * hm.width) + 'px';
+	hm.style.top = random(0, window.innerHeight - 2 * hm.height) + 'px';
 
-		setTimeout(elementRemover(hm), hmLifetime);
+	setTimeout(elementRemover(hm), hmLifetime);
 
-		document.body.appendChild(hm);
-	}, hmDelay);
+	document.body.appendChild(hm);
+
+	hmSoundElement.play();
 }
 
 // random in [min, max)
